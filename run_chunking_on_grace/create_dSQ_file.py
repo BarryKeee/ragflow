da23863e529b0131c1051e7f4b0f3506ac8f5bc8
Barry Ke <35748027+BarryKeee@users.noreply.github.com>
2024-07-10 12:01:46 -0400

    change setup

--- run_chunking_on_grace/create_dSQ_file.py
+++ run_chunking_on_grace/create_dSQ_file.py
@@ -14,10 +14,10 @@ partition_run_folder = os.path.join(BASE_FOLDER, pdf_folder_name + '_partition')
 
 # dsq file for scavenge_gpu
 file_name = f"run_ragflow_api_calls.txt"
-print('dsq --job-file ' + file_name + ' --cpus-per-task=1 --mem=8G --time=1-00:00:00 --output=JOBLOG_ragflow_parsing/dsq-jobfile-%A_%a-%N.out -p scavenge')
+print('dsq --job-file ' + file_name + ' --cpus-per-task=4 --mem=16G --time=1-00:00:00 --output=JOBLOG_ragflow_parsing/dsq-jobfile-%A_%a-%N.out -p scavenge')
 file1 = open(file_name, "w")
 for parid_file in os.listdir(partition_run_folder):
     parid = parid_file.split('_')[1]
-    line = f"module load miniconda; conda activate ragflow; python ragflow_chunking.py " \
+    line = f"export HF_ENDPOINT=https://hf-mirror.com; module load miniconda; conda activate ragflow; python ragflow_chunking.py " \
            f"{pdf_folder_name} {parid}\n"
     file1.writelines(line)
